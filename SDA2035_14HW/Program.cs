2ed29608d7d569e8fb3b812ad59329dadbebca4d
holonso <d1mo@live.com>
2021-12-16 23:47:38 +0900

    Добавьте файлы проекта.

---
+++ SDA2035_14HW/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA2035_14HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+
+            cat.ShowInfo();
+
+            Console.WriteLine();
+
+            dog.ShowInfo();
+
+            Console.ReadKey();
+        }
+
+        abstract class Animal
+        {
+            public abstract string Name { get; set; }
+            public Animal()
+            {
+                Name = "Никак";
+            }
+            public abstract void Say();
+            public void ShowInfo()
+            {
+                Console.WriteLine("Меня зовут - {0}", Name);
+                Say();
+            }
+        }
+        class Cat : Animal
+        {
+            string name;
+
+            public override string Name
+            {
+                get { return name; }
+                set { name = "Кошка"; }
+            }
+            public override void Say() { Console.WriteLine("Мяу"); }
+        }
+        class Dog : Animal
+        {
+            string name;
+            public override string Name
+            {
+                get { return name; }
+                set { name = "Собака"; }
+            }
+            public override void Say() { Console.WriteLine("Гав"); }
+        }
+    }
+}
